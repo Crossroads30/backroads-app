7d485256b9ca46f4f88871d4ab8b38c87fe44197
crossroads30 <crossroads30@mail.ru>
2023-12-27 12:44:55 +0300

    feat: add service data

--- src/data.js
+++ src/data.js
@@ -37,3 +37,23 @@ export const socialLinks = [
 		icon: 'fab fa-squarespace',
 	},
 ]
+export const services = [
+	{
+		id: 1,
+		title: 'Saving Money',
+		icon: 'fas fa-wallet fa-fw',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	},
+	{
+		id: 2,
+		title: 'Endless Hiking',
+		icon: 'fas fa-tree fa-fw',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	},
+	{
+		id: 3,
+		title: 'Amazing Comfort',
+		icon: 'fas fa-socks fa-fw',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+	},
+]
