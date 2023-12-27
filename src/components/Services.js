7e4ba372bdf86aa5e7cb982008e7626a1ef8a83e
crossroads30 <crossroads30@mail.ru>
2023-12-27 12:45:26 +0300

    refactor: add map for service articles

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,48 +1,27 @@
-import Title from "./Title"
+import { services } from '../data'
+import Title from './Title'
 
 function Services() {
-  return (
+	return (
 		<section className='section services' id='services'>
 			<Title title='our' subtitle='services' />
 			<div className='section-center services-center'>
-				<article className='service'>
-					<span className='service-icon'>
-						<i className='fas fa-wallet fa-fw'></i>
-					</span>
-					<div className='service-info'>
-						<h4 className='service-title'>saving money</h4>
-						<p className='service-text'>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Asperiores, officia.
-						</p>
-					</div>
-				</article>
-
-				<article className='service'>
-					<span className='service-icon'>
-						<i className='fas fa-tree fa-fw'></i>
-					</span>
-					<div className='service-info'>
-						<h4 className='service-title'>endless hiking</h4>
-						<p className='service-text'>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Asperiores, officia.
-						</p>
-					</div>
-				</article>
-
-				<article className='service'>
-					<span className='service-icon'>
-						<i className='fas fa-socks fa-fw'></i>
-					</span>
-					<div className='service-info'>
-						<h4 className='service-title'>amazing comfort</h4>
-						<p className='service-text'>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Asperiores, officia.
-						</p>
-					</div>
-				</article>
+				{services.map(service => {
+					const { id, title, icon, text } = service
+					return (
+						<article key={id} className='service'>
+							<span className='service-icon'>
+								<i className={icon}></i>
+							</span>
+							<div className='service-info'>
+								<h4 className='service-title'>{title}</h4>
+								<p className='service-text'>
+									{text}
+								</p>
+							</div>
+						</article>
+					)
+				})}
 			</div>
 		</section>
 	)
