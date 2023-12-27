3a928d0f9867cbc8eb4d14cce9c70035fe357d5a
crossroads30 <crossroads30@mail.ru>
2023-12-27 13:22:46 +0300

    feat: add date to 'tours'

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+
 export const pageLinks = [
 	{
 		id: 1,
@@ -60,42 +67,62 @@ export const services = [
 export const tours = [
 	{
 		id: 1,
-		image: '',
+		image: tour1,
 		tourDate: 'August 26th, 2020',
 		title: 'Tibet Adventure',
 		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
 		location: 'China',
 		duration: 6,
-		price: 2100,
+		cost: 2100,
 	},
 	{
 		id: 2,
-		image: '',
+		image: tour2,
 		tourDate: 'October 1th, 2020',
 		title: 'Best Of Java',
 		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-		location: '',
-		duration: '',
-		price: '',
+		location: 'indonesia',
+		duration: 11,
+		cost: 1400,
 	},
 	{
 		id: 3,
-		image: '',
+		image: tour3,
 		tourDate: 'September 15th, 2020',
 		title: 'Explore Hong Kong',
 		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-		location: '',
-		duration: '',
-		price: '',
+		location: 'hong kong',
+		duration: 8,
+		cost: 5000,
 	},
 	{
 		id: 4,
-		image: '',
+		image: tour4,
 		tourDate: 'December 5th, 2019',
 		title: 'Kenya Highlights',
 		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-		location: '',
-		daysCount: '',
-		price: '',
+		location: 'kenya',
+		daysCount: 20,
+		cost: 3300,
+	},
+	{
+		id: 5,
+		image: tour5,
+		tourDate: 'July 22th, 2019',
+		title: 'Africa Exploration',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: 'africa',
+		daysCount: 21,
+		cost: 6300,
+	},
+	{
+		id: 6,
+		image: tour6,
+		tourDate: 'November 5th, 2019',
+		title: 'India Travel',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: 'india',
+		daysCount: 13,
+		cost: 2300,
 	},
 ]
