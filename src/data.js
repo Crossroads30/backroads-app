3433b6ea1a8ec362792f32a819e3647146cd1484
crossroads30 <crossroads30@mail.ru>
2023-12-27 13:01:45 +0300

    feat: add tour data

--- src/data.js
+++ src/data.js
@@ -42,18 +42,60 @@ export const services = [
 		id: 1,
 		title: 'Saving Money',
 		icon: 'fas fa-wallet fa-fw',
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
 	},
 	{
 		id: 2,
 		title: 'Endless Hiking',
 		icon: 'fas fa-tree fa-fw',
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
 	},
 	{
 		id: 3,
 		title: 'Amazing Comfort',
 		icon: 'fas fa-socks fa-fw',
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+	},
+]
+export const tours = [
+	{
+		id: 1,
+		image: '',
+		tourDate: 'August 26th, 2020',
+		title: 'Tibet Adventure',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: 'China',
+		duration: 6,
+		price: 2100,
+	},
+	{
+		id: 2,
+		image: '',
+		tourDate: 'October 1th, 2020',
+		title: 'Best Of Java',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: '',
+		duration: '',
+		price: '',
+	},
+	{
+		id: 3,
+		image: '',
+		tourDate: 'September 15th, 2020',
+		title: 'Explore Hong Kong',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: '',
+		duration: '',
+		price: '',
+	},
+	{
+		id: 4,
+		image: '',
+		tourDate: 'December 5th, 2019',
+		title: 'Kenya Highlights',
+		info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+		location: '',
+		daysCount: '',
+		price: '',
 	},
 ]
